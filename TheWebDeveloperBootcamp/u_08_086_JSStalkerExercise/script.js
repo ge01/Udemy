2c825d46fd83c18af167e3d0a6a5ba05deca94e0
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-03-21 21:40:00 -0500

    JS stalker exercise

---
+++ TheWebDeveloperBootcamp/u_08_086_JSStalkerExercise/script.js
@@ -0,0 +1,8 @@
+// Request user information.
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var userAge = prompt("What is you age?");
+
+// Output user information in a sentance.
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("Your are " + userAge + " years old");
