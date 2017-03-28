554135ee717df63f991b1ddd988a3481047ed298
Gabe Escobedo <gabriel.escobedo01@utrgv.edu>
2017-03-28 18:47:18 -0500

    added 11_123 Quick Array Interation Challenge

--- TheWebDeveloperBootcamp/u_11_122_ArrayIteration/script.js
+++ TheWebDeveloperBootcamp/u_11_122_ArrayIteration/script.js
@@ -3,12 +3,12 @@ var colors = ["red", "orange", "yellow", "green"];
 
 for(var i = 0; i < colors.length; i++) {
 	console.log(colors[i]);
-}
+};
 
 console.log(" ");
 
 // using forEach
-colors.forEach(function(anything){
+colors.forEach(function(anything) {
 	console.log(anything);
 });
 
@@ -20,3 +20,33 @@ while( j < colors.length) {
 	console.log(colors[j]);
 	j++;
 }
+
+console.log(" ");
+
+// using forEach
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+numbers.forEach(function(HDTV) {
+	if(HDTV%3 === 0){
+		console.log(HDTV);
+	}
+});
+
+console.log(" ");
+
+// with a for loop
+for(var a = 0; a < numbers.length; a++) {
+	if(numbers[a]%3 === 0) {
+		console.log(numbers[a]);
+	}
+};
+
+console.log(" ");
+
+// using while loop
+var b = 0;
+while(b < numbers.length) {
+	if(numbers[b]% 3 === 0) {
+		console.log(numbers[b]);
+	}	
+	b++;
+};
