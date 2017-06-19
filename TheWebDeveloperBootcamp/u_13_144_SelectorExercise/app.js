667816701a86997861f21f75e916e52f6c970266
ge01 <gabriel.escobedo01@utrgv.edu>
2017-06-19 01:45:26 -0500

    selector exercise

---
+++ TheWebDeveloperBootcamp/u_13_144_SelectorExercise/app.js
@@ -0,0 +1,18 @@
+var element1 = document.getElementById("first");
+var element2 = document.querySelector("#first");
+var element3 = document.getElementsByClassName("special")[0];
+var element4 = document.querySelector(".special");
+var element5 = document.querySelectorAll(".special")[0];
+var element6 = document.getElementsByTagName("p")[0];
+var element7 = document.querySelector("p");
+var element8 = document.querySelectorAll("p")[0];
+
+
+console.log(element1);
+console.log(element2);
+console.log(element3);
+console.log(element4);
+console.log(element5);
+console.log(element6);
+console.log(element7);
+console.log(element8);
