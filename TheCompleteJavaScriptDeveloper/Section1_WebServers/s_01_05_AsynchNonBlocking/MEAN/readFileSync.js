1e7e67f7e5bdf39072bbb718e9782a9697aee7bf
ge01 <gabriel.escobedo01@utrgv.edu>
2017-07-03 00:04:01 -0500

    read file asynchronosly

--- TheCompleteJavaScriptDeveloper/Section1_WebServers/s_01_05_AsynchNonBlocking/MEAN/readFileSync.js
+++ TheCompleteJavaScriptDeveloper/Section1_WebServers/s_01_05_AsynchNonBlocking/MEAN/readFileSync.js
@@ -1,7 +1,13 @@
 var fs = require('fs');
 
 console.log("Going to get a file");
-var file = fs.readFileSync('readFileSync.js');
-console.log("Got the file");
+
+// Read file synchronosly
+//var file = fs.readFileSync('readFileSync.js');
+
+// Read file asynchronosly
+fs.readFile('readFileSync.js', function(err, file) {
+  console.log("Got the file");
+});
 
 console.log("App continuess...");
