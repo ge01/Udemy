01f4d213a6683d00f89bde610ab534fa0b0b9096
ge01 <gabriel.escobedo01@utrgv.edu>
2017-07-05 23:00:04 -0500

    creating the color game pt. 3

--- TheWebDeveloperBootcamp/u_15_160_ColorGameProject/colorGame.js
+++ TheWebDeveloperBootcamp/u_15_160_ColorGameProject/colorGame.js
@@ -7,8 +7,9 @@ var colors = [
   "rgb(255, 0, 255)"
 ];
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickedColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -22,10 +23,25 @@ for(var i = 0; i < squares.length; i++) {
     var clickedColor = this.style.backgroundColor;
     //compare color to pickedColor
     if(clickedColor === pickedColor) {
-      alert("Correct!");
+      messageDisplay.textContent = "Correct!";
+      changeColors(clickedColor);
     } else {
-      alert("Wrong!");
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again";
     }
   });
+}
+
+function changeColors(color) {
+  //loop through all squares
+  for(var i = 0; i < squares.length; i++) {
+    //change each color to match given color
+    squares[i].style.background = color;
+  }
+}
+
+function pickedColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
 // style.backgroundColor
