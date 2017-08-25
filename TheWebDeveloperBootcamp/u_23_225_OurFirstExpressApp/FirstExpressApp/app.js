288c8a2c80635ca91d65d4e6a0d45e99c62cf2b9
ge01 <gabriel.escobedo01@utrgv.edu>
2017-08-25 04:12:57 -0500

    Route Params

--- TheWebDeveloperBootcamp/u_23_225_OurFirstExpressApp/FirstExpressApp/app.js
+++ TheWebDeveloperBootcamp/u_23_225_OurFirstExpressApp/FirstExpressApp/app.js
@@ -14,6 +14,20 @@ app.get("/dog", function(req, res){
   console.log("SOMEONE MADE A REQUEST TO /DOG!!!");
   res.send("MEOW!");
 });
+
+app.get("/r/:subredditName", function(req, res){
+  var subreddit = req.params.subredditName;
+  res.send("WELCOME TO THE " + subreddit.toUpperCase() + " SUBREDDIT!");
+});
+
+app.get("/r/:subredditName/comments/:id/:title/", function(req, res){
+  res.send("WELCOME TO COMMENTS PAGE");
+});
+
+app.get("*", function(req, res){
+  res.send("YOU ARE A STAR!!!");
+});
+
 // Tell Express to listen for requests (start server)
 
 app.listen(3000, function(){
